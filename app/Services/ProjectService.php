553fecbeba1164196837a3bcb200df33913acf2f
Joao R C Almeida <joaorca@gmail.com>
2015-08-17 23:41:55 -0300

    [Aula 5.2] Criando o service ProjectService e o validator ProjectValidator

---
+++ app/Services/ProjectService.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace CodeProject\Services;
+
+use CodeProject\Repositories\ProjectRepository;
+use CodeProject\Validators\ProejctValidator;
+use Prettus\Validator\Exceptions\ValidatorException;
+
+class ProjectService
+{
+    /**
+     * @var ProjectRepository
+     */
+    protected $repository;
+
+    /**
+     * @var ProejctValidator
+     */
+    private $validator;
+
+    /**
+     * @param ProjectRepository $repository
+     * @param ProejctValidator $validator
+     */
+    public function __construct(
+        ProjectRepository $repository,
+        ProejctValidator $validator
+    ) {
+        $this->repository = $repository;
+        $this->validator = $validator;
+    }
+
+    /**
+     * @param array $data
+     * @return mixed
+     */
+    public function create(array $data)
+    {
+        try {
+            $this->validator->with($data)->passesOrFail();
+
+            return $this->repository->create($data);
+
+        } catch (ValidatorException $e) {
+            return [
+                'error'   => true,
+                'message' => $e->getMessageBag(),
+            ];
+        }
+    }
+
+    /**
+     * @param array $data
+     * @param $id
+     * @return mixed
+     */
+    public function update(array $data, $id)
+    {
+        try {
+            $this->validator->with($data)->passesOrFail();
+
+            return $this->repository->update($data, $id);
+
+        } catch (ValidatorException $e) {
+            return [
+                'error'   => true,
+                'message' => $e->getMessageBag(),
+            ];
+        }
+    }
+
+}
