a7bb806474d2b65af7b5fdc85e26ce67b701714f
Joao R C Almeida <joaorca@gmail.com>
2015-08-18 23:50:04 -0300

    [Aula 5.6] Criando a API para ProjectNote

---
+++ app/Http/Controllers/ProjectNoteController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace CodeProject\Http\Controllers;
+
+use CodeProject\Repositories\ProjectNoteRepository;
+use CodeProject\Services\ProjectNoteService;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class ProjectNoteController extends Controller
+{
+    /**
+     * @var ProjectNoteRepository
+     */
+    private $repository;
+    /**
+     * @var ProjectNoteService
+     */
+    private $service;
+
+    /**
+     * @param ProjectNoteRepository $repository
+     * @param ProjectNoteService $service
+     */
+    public function __construct(
+        ProjectNoteRepository $repository,
+        ProjectNoteService $service
+    ) {
+        $this->repository = $repository;
+        $this->service = $service;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @param $id
+     * @return Response
+     */
+    public function index($id)
+    {
+        return $this->repository->findWhere(['project_id' => $id]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  Request $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        return $this->service->create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @param $noteId
+     * @return Response
+     */
+    public function show($id, $noteId)
+    {
+        return $this->repository->findWhere(['project_id'=>$id,'id'=>$noteId]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  Request $request
+     * @param  int $id
+     * @param $noteId
+     * @return Response
+     */
+    public function update(Request $request, $id, $noteId)
+    {
+        $this->service->update($request->all(), $noteId);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @param $noteId
+     * @return Response
+     */
+    public function destroy($id, $noteId)
+    {
+        $this->repository->delete($noteId);
+    }
+
+}
